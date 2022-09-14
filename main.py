2c4b3dcaa386c0716771c4bc4ee813606f88090b
Mihir Gupta <69476837+gupta-mihir@users.noreply.github.com>
2022-09-14 11:03:04 -0400

    new func where computer guesses num and user ans

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ def guess(x):
             print('Sorry, guess again, too high')
     print(f'Yay, you guessed {random_number} correctly!')
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else: 
+            guess = low
+        #guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high (H) or too low (L) or correct (C)?').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'Yay! The computer guessed your number, {guess} correctly!') 
+
+computer_guess(10)     
 
 
